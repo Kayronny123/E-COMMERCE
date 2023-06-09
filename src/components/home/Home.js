f2686d9cd6a95af444394b2bc5695b5df74f697e
Rogerio-kayronny-sales <108687095+Kayronny123@users.noreply.github.com>
2023-06-09 18:55:13 -0300

    Corrigindo função de add ao carrinho

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,10 +2,22 @@ import foquete from "../../foquete.json";
 import { FoqueteContainer, Imagem } from "./style";
 
 const Home = ({ carrinho, setCarrinho }) => {
-  console.log(foquete);
   const addFoqueteCarrinho = (foquete) => {
-    setCarrinho([...carrinho], foquete);
+    const foqueteExisteCarrinho = carrinho.find(
+      (item) => item.nome === foquete.nome
+    );
+    if (foqueteExisteCarrinho) {
+      const novoCarrinho = carrinho.map((item) =>
+        foqueteExisteCarrinho.nome === item.nome
+          ? { ...item, quantidade: item.quantidade + 1 }
+          : item
+      );
+      setCarrinho(novoCarrinho);
+    } else {
+      setCarrinho([...carrinho, { ...foquete, quantidade: 1 }]);
+    }
   };
+  console.log(carrinho, "add carrinho");
 
   return (
     <div>
@@ -13,7 +25,7 @@ const Home = ({ carrinho, setCarrinho }) => {
         return (
           <FoqueteContainer key={foquete.id}>
             <Imagem src={foquete.image} alt={foquete.name} />
-            <p>Nome: {foquete.name}</p>
+            <p>{foquete.name}</p>
             <p>Preço: {foquete.valor}</p>
             <button onClick={() => addFoqueteCarrinho(foquete)}>Comprar</button>
           </FoqueteContainer>
